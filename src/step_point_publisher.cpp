3040d509ea9966674d4d26da19e1086b2422c4bf
OnoFumiya <fumiyaono.choi@gmail.com>
2024-07-15 18:29:13 +0900

    Update

--- src/step_point_publisher.cpp
+++ src/step_point_publisher.cpp
@@ -20,6 +20,9 @@
 #include <navigation_stack/WalkLegPoint.h>
 #include <tf/transform_listener.h>
 
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/time_synchronizer.h>
 
 #include <algorithm>
 
@@ -28,83 +31,129 @@ using namespace Eigen;
 
 // using namespace std;
 
+typedef message_filters::sync_policies::ApproximateTime<dr_spaam_ros::LegPoseArray, sensor_msgs::LaserScan> DrspaamScanSyncPolicy;
+
+
 class ALL_PARAMETER
 {
+    private:
+        ros::NodeHandle nh_, pnh_;
     public:
         float lidar_pose[2] = {0.2, 0.0};
-        int sampling_step = 5;
-        float max_human_vel = 1.5;
-        float min_human_vel = 0.1;
-        float max_human_radius = 0.80;
-        float human_noise = 0.4;
-        float detect_range_time = 0.1;
-        float velocity_weight_prediction = 2.0;
-        float position_weight_prediction = 1.3333;
-        float similar_object = 0.2;
-        float no_moving_range = 0.6;
-        std::string robot_base = "base_footprint";
-        std::string map = "map";
-        std::string removal_scan_topic = "/dr_spaam_navigation/scan";
-        std::string stepping_point_topic = "/dr_spaam_navigation/object_pointers_step";
-        std::string dr_spaam_topic = "/dr_spaam_detections";
-        std::string merge_object_topic = "/object_point";
-        std::string removal_scan_marker = "/dr_spaam_navigation/lidar_points";
-        std::string step1_marker = "/dr_spaam_navigation/point_step1";
-        std::string step2_marker = "/dr_spaam_navigation/point_step2";
-        std::string step3_marker = "/dr_spaam_navigation/point_step3";
-        std::string next_step_marker = "/dr_spaam_navigation/point_nextstep";
-        std::string cost_topic = "/dr_spaam_navigation/point_cost";
-        ALL_PARAMETER()
+        // int sampling_step = 5;
+        // float max_human_vel = 1.5;
+        // float max_human_radius = 0.80;
+        // float human_noise = 0.4;
+        // float detect_range_time = 0.1;
+        // float velocity_weight_prediction = 2.0;
+        // float position_weight_prediction = 1.3333;
+        // float similar_object = 0.2;
+        // float no_moving_range = 0.6;
+        // std::string robot_base = "base_footprint";
+        // std::string map = "map";
+        // std::string removal_scan_topic = "/dr_spaam_navigation/scan";
+        // std::string stepping_point_topic = "/dr_spaam_navigation/object_pointers_step";
+        // std::string dr_spaam_topic = "/dr_spaam_detections";
+        // std::string merge_object_topic = "/object_point";
+        // std::string removal_scan_marker = "/dr_spaam_navigation/lidar_points";
+        // std::string step1_marker = "/dr_spaam_navigation/point_step1";
+        // std::string step2_marker = "/dr_spaam_navigation/point_step2";
+        // std::string step3_marker = "/dr_spaam_navigation/point_step3";
+        // std::string next_step_marker = "/dr_spaam_navigation/point_nextstep";
+        // std::string cost_topic = "/dr_spaam_navigation/point_cost";
+        int sampling_step;
+        float max_human_vel;
+        float min_human_vel;
+        float max_human_radius;
+        float human_noise;
+        float detect_range_time;
+        float velocity_weight_prediction;
+        float position_weight_prediction;
+        float similar_object;
+        float no_moving_range;
+        std::string robot_base;
+        std::string map;
+        std::string removal_scan_topic;
+        std::string stepping_point_topic;
+        std::string dr_spaam_topic;
+        std::string merge_object_topic;
+        std::string removal_scan_marker;
+        std::string step1_marker;
+        std::string step2_marker;
+        std::string step3_marker;
+        std::string next_step_marker;
+        std::string cost_topic;
+        ALL_PARAMETER() : nh_(), pnh_("~")
         {
-            ros::NodeHandle nh;
-            XmlRpc::XmlRpcValue param;
-            nh.getParam("lidar_pose", param);
-            lidar_pose[0] = static_cast<double>(param[0]);
-            lidar_pose[1] = static_cast<double>(param[1]);
-            nh.getParam("sampling_step", param);
-            sampling_step = static_cast<int>(param);
-            nh.getParam("max_human_vel", param);
-            max_human_vel = static_cast<double>(param);
-            nh.getParam("min_human_vel", param);
-            min_human_vel = static_cast<double>(param);
-            nh.getParam("max_human_radius", param);
-            max_human_radius = static_cast<double>(param);
-            nh.getParam("human_noise", param);
-            human_noise = static_cast<double>(param);
-            nh.getParam("velocity_weight_prediction", param);
-            velocity_weight_prediction = static_cast<double>(param);
-            nh.getParam("position_weight_prediction", param);
-            position_weight_prediction = static_cast<double>(param);
-            nh.getParam("similar_object", param);
-            similar_object = static_cast<double>(param);
-            nh.getParam("no_moving_range", param);
-            no_moving_range = static_cast<double>(param);
-            nh.getParam("detect_range_time", param);
-            detect_range_time = static_cast<double>(param);
-            nh.getParam("robot_base", param);
-            robot_base = static_cast<std::string>(param);
-            nh.getParam("map", param);
-            map = static_cast<std::string>(param);
-            nh.getParam("removal_scan_topic", param);
-            removal_scan_topic = static_cast<std::string>(param);
-            nh.getParam("stepping_point_topic", param);
-            stepping_point_topic = static_cast<std::string>(param);
-            nh.getParam("dr_spaam_topic", param);
-            dr_spaam_topic = static_cast<std::string>(param);
-            nh.getParam("merge_object_topic", param);
-            merge_object_topic = static_cast<std::string>(param);
-            nh.getParam("removal_scan_marker", param);
-            removal_scan_marker = static_cast<std::string>(param);
-            nh.getParam("step1_marker", param);
-            step1_marker = static_cast<std::string>(param);
-            nh.getParam("step2_marker", param);
-            step2_marker = static_cast<std::string>(param);
-            nh.getParam("step3_marker", param);
-            step3_marker = static_cast<std::string>(param);
-            nh.getParam("next_step_marker", param);
-            next_step_marker = static_cast<std::string>(param);
-            nh.getParam("cost_topic", param);
-            cost_topic = static_cast<std::string>(param);
+            sampling_step = nh_.param<float>( "sampling_step", 0.00 );
+            max_human_vel = nh_.param<float>( "max_human_vel", 0.00 );
+            min_human_vel = nh_.param<float>( "min_human_vel", 0.00 );
+            max_human_radius = nh_.param<float>( "max_human_radius", 0.00 );
+            human_noise = nh_.param<float>( "human_noise", 0.00 );
+            velocity_weight_prediction = nh_.param<float>( "velocity_weight_prediction", 0.00 );
+            position_weight_prediction = nh_.param<float>( "position_weight_prediction", 0.00 );
+            similar_object = nh_.param<float>( "similar_object", 0.00 );
+            no_moving_range = nh_.param<float>( "no_moving_range", 0.00 );
+            detect_range_time = nh_.param<float>( "detect_range_time", 0.00 );
+            robot_base = nh_.param<std::string>( "robot_base", "dummy" );
+            map = nh_.param<std::string>( "map", "dummy" );
+            removal_scan_topic = nh_.param<std::string>( "removal_scan_topic", "dummy" );
+            stepping_point_topic = nh_.param<std::string>( "stepping_point_topic", "dummy" );
+            dr_spaam_topic = nh_.param<std::string>( "dr_spaam_topic", "dummy" );
+            merge_object_topic = nh_.param<std::string>( "merge_object_topic", "dummy" );
+            removal_scan_marker = nh_.param<std::string>( "removal_scan_marker", "dummy" );
+            step1_marker = nh_.param<std::string>( "step1_marker", "dummy" );
+            step2_marker = nh_.param<std::string>( "step2_marker", "dummy" );
+            step3_marker = nh_.param<std::string>( "step3_marker", "dummy" );
+            next_step_marker = nh_.param<std::string>( "next_step_marker", "dummy" );
+            cost_topic = nh_.param<std::string>( "cost_topic", "dummy" );
+            // ros::NodeHandle nh;
+            // XmlRpc::XmlRpcValue param;
+            // nh.getParam("lidar_pose", param);
+            // lidar_pose[0] = static_cast<double>(param[0]);
+            // lidar_pose[1] = static_cast<double>(param[1]);
+            // nh.getParam("sampling_step", param);
+            // sampling_step = static_cast<int>(param);
+            // nh.getParam("max_human_vel", param);
+            // max_human_vel = static_cast<double>(param);
+            // nh.getParam("max_human_radius", param);
+            // max_human_radius = static_cast<double>(param);
+            // nh.getParam("human_noise", param);
+            // human_noise = static_cast<double>(param);
+            // nh.getParam("velocity_weight_prediction", param);
+            // velocity_weight_prediction = static_cast<double>(param);
+            // nh.getParam("position_weight_prediction", param);
+            // position_weight_prediction = static_cast<double>(param);
+            // nh.getParam("similar_object", param);
+            // similar_object = static_cast<double>(param);
+            // nh.getParam("no_moving_range", param);
+            // no_moving_range = static_cast<double>(param);
+            // nh.getParam("detect_range_time", param);
+            // detect_range_time = static_cast<double>(param);
+            // nh.getParam("robot_base", param);
+            // robot_base = static_cast<std::string>(param);
+            // nh.getParam("map", param);
+            // map = static_cast<std::string>(param);
+            // nh.getParam("removal_scan_topic", param);
+            // removal_scan_topic = static_cast<std::string>(param);
+            // nh.getParam("stepping_point_topic", param);
+            // stepping_point_topic = static_cast<std::string>(param);
+            // nh.getParam("dr_spaam_topic", param);
+            // dr_spaam_topic = static_cast<std::string>(param);
+            // nh.getParam("merge_object_topic", param);
+            // merge_object_topic = static_cast<std::string>(param);
+            // nh.getParam("removal_scan_marker", param);
+            // removal_scan_marker = static_cast<std::string>(param);
+            // nh.getParam("step1_marker", param);
+            // step1_marker = static_cast<std::string>(param);
+            // nh.getParam("step2_marker", param);
+            // step2_marker = static_cast<std::string>(param);
+            // nh.getParam("step3_marker", param);
+            // step3_marker = static_cast<std::string>(param);
+            // nh.getParam("next_step_marker", param);
+            // next_step_marker = static_cast<std::string>(param);
+            // nh.getParam("cost_topic", param);
+            // cost_topic = static_cast<std::string>(param);
         }
 };
 
@@ -123,6 +172,11 @@ class OBJECT_DETECT
         std::vector<geometry_msgs::Point> object_points;
         std::vector<geometry_msgs::Point> points_base;
         geometry_msgs::Twist robot_vel;
+
+        std::unique_ptr<message_filters::Subscriber<dr_spaam_ros::LegPoseArray>> sub_legs_;
+        std::unique_ptr<message_filters::Subscriber<sensor_msgs::LaserScan>>     sub_scan_;
+
+        std::shared_ptr<message_filters::Synchronizer<DrspaamScanSyncPolicy>>    sync_;
         geometry_msgs::Point Pointtransform(const std::string& org_frame, const std::string& target_frame, const geometry_msgs::Point& point)
         {
             geometry_msgs::PointStamped ptstanp_transformed;
@@ -147,17 +201,19 @@ class OBJECT_DETECT
             }
             return pt_transformed;
         }
-        void callback_object(const dr_spaam_ros::LegPoseArray &msg)
+        // void callback_object(const dr_spaam_ros::LegPoseArray &msg)
+        void callback_object(const dr_spaam_ros::LegPoseArrayConstPtr &msg, 
+                             const sensor_msgs::LaserScanConstPtr     &scan_msg)
         {
             object_points.clear();
             points_base.clear();
             geometry_msgs::Point point;
-            for (int i=0; i<msg.poses.size(); i++)
+            for (int i=0; i<msg->poses.size(); i++)
             {
-                point.x = msg.poses[i].position.x + all_parameter.lidar_pose[0];
-                point.y = msg.poses[i].position.y + all_parameter.lidar_pose[1];
+                point.x = msg->poses[i].position.x + all_parameter.lidar_pose[0];
+                point.y = msg->poses[i].position.y + all_parameter.lidar_pose[1];
                 point.z = 0.2;
-                if ((0.25 <= sqrtf(powf(point.x, 2.) + powf(point.y, 2.))) && (sqrtf(powf(point.x, 2.) + powf(point.y, 2.)) <= msg.scan.range_max))
+                if ((0.25 <= sqrtf(powf(point.x, 2.) + powf(point.y, 2.))) && (sqrtf(powf(point.x, 2.) + powf(point.y, 2.)) <= scan_msg->range_max))
                 {
                     points_base.push_back(point);
                     object_points.push_back(Pointtransform(all_parameter.robot_base, all_parameter.map, point));
@@ -165,26 +221,25 @@ class OBJECT_DETECT
             }
 
             sensor_msgs::LaserScan removal_lidar_data;
-            removal_lidar_data.header.seq = msg.scan.header.seq;
-            removal_lidar_data.header.stamp = msg.scan.header.stamp;
-            removal_lidar_data.header.frame_id = msg.scan.header.frame_id;
-            removal_lidar_data.angle_min = msg.scan.angle_min;
-            removal_lidar_data.angle_max = msg.scan.angle_max;
-            removal_lidar_data.angle_increment = msg.scan.angle_increment;
-            removal_lidar_data.time_increment = msg.scan.time_increment;
-            removal_lidar_data.scan_time = msg.scan.scan_time;
-            removal_lidar_data.range_min = msg.scan.range_min;
-            removal_lidar_data.range_max = msg.scan.range_max;
-            removal_lidar_data.ranges.resize(msg.scan.ranges.size());
-            copy(msg.scan.ranges.begin(), msg.scan.ranges.end(), removal_lidar_data.ranges.begin());
-            float range_angle_increment = msg.scan.angle_increment;
-            for (int i=0; i<msg.scan.ranges.size(); i++)
+            removal_lidar_data.header.seq = scan_msg->header.seq;
+            removal_lidar_data.header.stamp = scan_msg->header.stamp;
+            removal_lidar_data.header.frame_id = scan_msg->header.frame_id;
+            removal_lidar_data.angle_min = scan_msg->angle_min;
+            removal_lidar_data.angle_max = scan_msg->angle_max;
+            removal_lidar_data.angle_increment = scan_msg->angle_increment;
+            removal_lidar_data.time_increment = scan_msg->time_increment;
+            removal_lidar_data.scan_time = scan_msg->scan_time;
+            removal_lidar_data.range_min = scan_msg->range_min;
+            removal_lidar_data.range_max = scan_msg->range_max;
+            removal_lidar_data.ranges.resize(scan_msg->ranges.size());
+            copy(scan_msg->ranges.begin(), scan_msg->ranges.end(), removal_lidar_data.ranges.begin());
+            for (int i=0; i<scan_msg->ranges.size(); i++)
             {
-                if ((msg.scan.range_min <= msg.scan.ranges[i]) && (msg.scan.ranges[i] <= msg.scan.range_max))
+                if ((scan_msg->range_min <= scan_msg->ranges[i]) && (scan_msg->ranges[i] <= scan_msg->range_max))
                 {
                     for (int j=0; j<points_base.size(); j++)
                     {
-                        if (sqrtf(powf((msg.scan.ranges[i]*(cos(msg.scan.angle_min + range_angle_increment*i)) + all_parameter.lidar_pose[0]) - points_base[j].x, 2.) + powf((msg.scan.ranges[i]*(sin(msg.scan.angle_min + range_angle_increment*i)) + all_parameter.lidar_pose[1]) - points_base[j].y, 2.)) <= all_parameter.max_human_radius)
+                        if (sqrtf(powf((scan_msg->ranges[i]*(cos(scan_msg->angle_min + scan_msg->angle_increment*i)) + all_parameter.lidar_pose[0]) - points_base[j].x, 2.) + powf((scan_msg->ranges[i]*(sin(scan_msg->angle_min + scan_msg->angle_increment*i)) + all_parameter.lidar_pose[1]) - points_base[j].y, 2.)) <= all_parameter.max_human_radius)
                         {
                             removal_lidar_data.ranges[i] = NAN;
                             break;
@@ -207,10 +262,17 @@ class OBJECT_DETECT
     public:
         OBJECT_DETECT()
         {
+            robotvel_sub = nh.subscribe("mobile_base/commands/velocity", 10, &OBJECT_DETECT::callback_robotvel, this);
             pub_step_point = nh.advertise<navigation_stack::WalkLegPoint>(all_parameter.stepping_point_topic, 10);
             pub_pointremoval_scan = nh.advertise<sensor_msgs::LaserScan>(all_parameter.removal_scan_topic, 10);
-            object_sub = nh.subscribe(all_parameter.merge_object_topic, 10, &OBJECT_DETECT::callback_object, this);
-            robotvel_sub = nh.subscribe("mobile_base/commands/velocity", 10, &OBJECT_DETECT::callback_robotvel, this);
+            // object_sub = nh.subscribe(all_parameter.merge_object_topic, 10, &OBJECT_DETECT::callback_object, this);
+
+            sub_legs_.reset(new message_filters::Subscriber<dr_spaam_ros::LegPoseArray>(nh, all_parameter.merge_object_topic, 5));
+            sub_scan_.reset(new message_filters::Subscriber<sensor_msgs::LaserScan>(nh, "/scan", 5));
+            
+            sync_.reset(new message_filters::Synchronizer<DrspaamScanSyncPolicy>(DrspaamScanSyncPolicy(200), *sub_legs_, *sub_scan_));
+            sync_->registerCallback(boost::bind(&OBJECT_DETECT::callback_object, this, _1, _2));
+
             robot_vel.linear.x = 0.;
             robot_vel.linear.y = 0.;
             robot_vel.linear.z = 0.;
